bbbdeed54ec1d68462731ff6017605aa8a6edbf1
David Murdoch <dsmurdoch357@gmail.com>
2024-10-06 14:42:23 +0100

    chore(stylelint): use kebab-case instead of camelCase in stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -12,21 +12,21 @@ const CUSTOM_AT_RULES = [
   "mixin",
 ];
 
-// Enforces certain selectors to be only in camelCase notation
+// Enforces certain selectors to be only in kebab-case notation
 // We use these for id selectors and classname selectors
-const ONLY_ALLOW_CAMEL_CASE_SELECTORS = [
-  /^(?:[a-z]+(?:[A-Z][a-z]*)*)$/,
-  { message: (s) => `Expected '${s}' to be in camelCase` },
+const ONLY_ALLOW_KEBAB_CASE_SELECTORS = [
+  /^(?:[a-z]+(?:-[a-z]+)*)$/,
+  { message: (s) => `Expected '${s}' to be in kebab-case` },
 ];
 
 export default {
   extends: ["stylelint-config-standard"],
   plugins: ["stylelint-order", "stylelint-selector-bem-pattern"],
   rules: {
-    // Enforces Element Class Names to be camelCase
-    "selector-class-pattern": ONLY_ALLOW_CAMEL_CASE_SELECTORS,
-    // Enforces Element IDs to be camelCase
-    "selector-id-pattern": ONLY_ALLOW_CAMEL_CASE_SELECTORS,
+    // Enforces Element Class Names to be kebab-case
+    "selector-class-pattern": ONLY_ALLOW_KEBAB_CASE_SELECTORS,
+    // Enforces Element IDs to be kebab-case
+    "selector-id-pattern": ONLY_ALLOW_KEBAB_CASE_SELECTORS,
     // Allow Tailwind-based CSS Rules
     "at-rule-no-unknown": [true, { ignoreAtRules: CUSTOM_AT_RULES }],
     // Allow the Global CSS Selector
@@ -34,8 +34,40 @@ export default {
       true,
       { ignorePseudoClasses: ["global"] },
     ],
-    // Enforces the order of the CSS properties to be in alphabetical order
-    "order/properties-alphabetical-order": true,
+    // cssDeclarationSorterOrder: smacss
+    "order/properties-order": [
+      {
+        properties: ["position", "top", "right", "bottom", "left", "z-index"],
+      },
+      {
+        properties: ["display", "visibility", "float", "clear"],
+      },
+      {
+        properties: [
+          "flex",
+          "flex-direction",
+          "flex-wrap",
+          "flex-flow",
+          "flex-basis",
+          "flex-grow",
+          "flex-shrink",
+          "justify-content",
+          "align-items",
+          "align-self",
+          "order",
+        ],
+      },
+      {
+        properties: [
+          "width",
+          "min-width",
+          "max-width",
+          "height",
+          "min-height",
+          "max-height",
+        ],
+      },
+    ],
     "no-descending-specificity": null,
     // Disables the Level-4 Media Queries; Since they're more exotic and less known
     "media-feature-range-notation": "prefix",
