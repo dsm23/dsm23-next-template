b61b3564d031b70f5edfcb5b271c44f399fa3612
David Murdoch <dsmurdoch357@gmail.com>
2025-01-13 13:45:17 +0000

    chore(playwright): update playwright config
    * includes dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,28 @@
+import fs from "node:fs";
+import path from "node:path";
 import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const injectFromEnvFile = () => {
+  const envDir = ".";
+  const envFiles = [
+    /** default file */ `.env`,
+    /** local file */ `.env.local`,
+    /** mode file */ `.env.playwright`,
+    /** mode local file */ `.env.playwright.local`,
+  ];
+
+  envFiles.forEach((file) => {
+    const filePath = path.join(envDir, file);
+    if (fs.existsSync(filePath)) {
+      dotenv.config({ path: filePath });
+    }
+  });
+};
+
+injectFromEnvFile();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -17,6 +35,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -24,7 +43,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: `http://127.0.0.1:${PORT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -71,7 +90,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "pnpm dev",
-    url: "http://127.0.0.1:3000",
+    url: `http://127.0.0.1:${PORT}`,
     reuseExistingServer: !process.env.CI,
   },
 });
