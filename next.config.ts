43e7f4318ef4c30f7d69a3bc89d8489861bafd1f
David Murdoch <dsmurdoch357@gmail.com>
2025-05-29 15:05:14 +0100

    chore(typed routes): add typed routes

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,9 @@ const nextConfig: NextConfig = {
             exclude: ["error", "warn"],
           },
         },
+  experimental: {
+    typedRoutes: true,
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
