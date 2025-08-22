e8139e86f9ab27feb1441d3992cedc8e0ed1ccf0
David Murdoch <dsmurdoch357@gmail.com>
2025-08-22 15:04:17 +0100

    chore(vitest): import.meta.dirname

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { defineConfig } from "vitest/config";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
@@ -17,7 +14,9 @@ export default defineConfig({
         plugins: [
           // The plugin will run tests for the stories defined in your Storybook config
           // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+          storybookTest({
+            configDir: path.join(import.meta.dirname, ".storybook"),
+          }),
         ],
         test: {
           name: "storybook",
